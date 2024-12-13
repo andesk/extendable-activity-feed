d9404a3ef52fc2b73edc4b9e68f078447a71e615
andesk <a.c.kleemann@gmail.com>
2024-12-13 23:55:44 +0100

    splitting repository interface into two

---
+++ src/Domain/Repositories/ExtendedActivityRepositoryInterface.php
@@ -0,0 +1,23 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Andesk\EAF\Domain\Domain\Repositories;
+
+use Andesk\EAF\Domain\BaseActivityInterface;
+use DateTimeImmutable;
+
+interface ExtendedActivityRepositoryInterface extends ActivityRepositoryInterface
+{
+    public function getFeedFromUserPeers(
+        string $userId, 
+        int $limit = 20, 
+        DateTimeImmutable $offsetDate = null
+    ): array;
+
+    public function getFeedFromUserNetwork(
+        string $userId, 
+        int $limit = 20, 
+        DateTimeImmutable $offsetDate = null
+    ): array;
+}
